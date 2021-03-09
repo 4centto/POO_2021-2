8c7db9f04e6f76daff81f4aa5a31613730bb498c
4centto <ivangarcia080902@gmail.com>
2021-03-09 15:21:31 -0600

    08-mar-2021

--- proyecto_inicial_poo-main/src/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
+++ proyecto_inicial_poo-main/src/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mx.unam.aragon.fes.persistencia;
 
 import java.util.ArrayList;
 import mx.unam.aragon.fes.Empleado;
 
-/**
- *
- * @author kevin
- */
 public class ArchivoEmpleado {
     private String archivo;
 
